431ce666f993cf878afc29e2e9671a08338e02d3
malizorba <97551928+malizorba@users.noreply.github.com>
2022-02-04 15:11:19 +0300

    StudeninformationSystem

---
+++ Course.java
@@ -0,0 +1,29 @@
+public class Course {
+    Teacher teacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+    int sozlunot;
+
+    Course(String name,String code,String prefix){
+        this.name = name;
+        this.code=code;
+        this.prefix=prefix;
+        int note = 0;
+        int sozlunot;
+    }
+
+    void addTeacher(Teacher teacher){
+        if (teacher.branch.equals(this.prefix)){
+            this.teacher=teacher;
+
+        }else{
+            System.out.println("öğretmen ve ders uyusmuyor");
+        }
+
+    }
+    void printTeacher(){
+       this.teacher.print();
+    }
+}
---
+++ Teacher.java
@@ -0,0 +1,17 @@
+public class Teacher {
+    String name;
+    String branch;
+    String mphone;
+
+    Teacher(String name,String branch,String mphone){
+        this.name = name;
+        this.branch=branch;
+        this.mphone=mphone;
+
+    }
+    void print (){
+        System.out.println("Adı  :"+this.name);
+        System.out.println("Branşı :"+this.branch);
+        System.out.println("Telefonu :"+mphone);
+    }
+}
